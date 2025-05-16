84d60db8f27a7d656033d6c946b2ffe6cbf7fcb9
Genrevvv <genrevvaguilar@gmail.com>
2025-05-16 19:12:26 +0800

    Fix bug on <p> tag encapsulation (again lol)

--- js/parse-md.js
+++ js/parse-md.js
@@ -7,14 +7,14 @@ function parse(input) {
         { regex: /^##### (.+)$/gm, replace: '<h5>$1</h5>' },
         { regex: /^###### (.+)$/gm, replace: '<h6>$1</h6>' },
         { regex: /^> (.+)$/gm, replace: '<span class="quote">$1</span>' },
-        { regex: /((?:\*\*?|__?)?(?:\[.+\]\(.+\)|.+)+(?:\*\*?|__?)?)/gm, replace: '<p>$1</p>' },
+        { regex: /^`([^`]+)`$/gm, replace: '<span class="inline-code">$1</span>' },
+        { regex: /^```([\s\S]+)```$/gm, replace: '<pre class="code-block">$1</pre>' },
+        { regex: /((?:__|\*\*|\*|_).+?(?:__|\*\*|\*|_))/gm, replace: '<p>$1</p>' },
         { regex: /\[(.+)\]\((.+)\)/gm, replace:'<a href="$2">$1</a>' },
         { regex: /\*\*(.+?)\*\*/gm, replace: '<strong>$1</strong>' },
         { regex: /__(.+?)__/gm, replace: '<strong>$1</strong>' },
         { regex: /\*(.+?)\*/gm, replace: '<em>$1</em>' },
-        { regex: /_(.+?)_/gm, replace: '<em>$1</em>' },
-        { regex: /^`([^`]+)`$/gm, replace: '<span class="inline-code">$1</span>' },
-        { regex: /^```([\s\S]+)```$/gm, replace: '<pre class="code-block">$1</pre>' },
+        { regex: /_(.+?)_/gm, replace: '<em>$1</em>' }
     ];
 
     for (const { regex , replace } of patterns) {
